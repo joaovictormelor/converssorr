169338e842674cfa900588976f37a15127ee8cc1
joao <joaovictormelodonascimento@gmail.com>
2023-08-20 22:16:36 -0300

    criação da interface

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int op;
+printf("Digite a opcao desejada:\n(1)Decimal para Binario");
+scanf("%d", &op);
+
+if(op==1){
+
+}
+
+    return 0;
+}
