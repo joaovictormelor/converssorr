b3cba9ef34c25b8db21e7e1b44d892c937162f64
joao <joaovictormelodonascimento@gmail.com>
2023-08-20 22:30:26 -0300

    adicionado dec2bin

--- main.c
+++ main.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void dec2bin(int num);
+
 int main(){
 int op;
-printf("Digite a opcao desejada:\n(1)Decimal para Binario");
+int numero;
+printf("Digite a opcao desejada:\n(1)Decimal para Binario: ");
 scanf("%d", &op);
 
 if(op==1){
-
+printf("Digite o numero Decimal a ser convertido em Binario: ");
+scanf("%d", &numero);
+dec2bin(numero);
 }
 
     return 0;
+}
+
+void dec2bin(int num){
+    int vet[50];
+    int i=0, j;
+    int aux = num;
+
+    while(num > 0){
+        vet[i] = num % 2;
+        i++;
+        num = num/2;
+    }
+    printf("%d em binario: ", aux);
+
+    for(j=i-1;j>=0;j--)
+        printf("%d", vet[j]);
 }
